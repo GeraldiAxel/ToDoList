c60f7d543915ed8fcba3633be5df449ef41d5ced
GeraldiAxel <geraldi.axel@gmail.com>
2023-08-21 18:44:35 +0700

    Added monthly task feature

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 let newTask = [];
+let newTaskMonthly = [];
 
 app.get("/", (req, res) => {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -21,6 +22,20 @@ app.post("/", (req, res) => {
         newTask.push(req.body["newItem"]);
     }
     res.redirect("/");
+});
+
+app.get("/monthly", (req, res) => {
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let today = new Date();
+    let month = months[today.getMonth()];
+    res.render("monthly.ejs", {currentDate : month, newMonthlyItem: newTaskMonthly});
+});
+
+app.post("/monthly", (req, res) => {
+    if(req.body["newMonthlyItem"]){
+        newTaskMonthly.push(req.body["newMonthlyItem"]);
+    }
+    res.redirect("/monthly");
 });
 
 app.listen(port, () => {
